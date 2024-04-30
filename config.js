efd6e4cf4dde642d38de9442ab362ef4d1930b6d
Jefferson10MR <165387779+Jefferson10MR@users.noreply.github.com>
2024-04-30 16:58:05 -0500

    Update config.js

--- config.js
+++ config.js
@@ -163,9 +163,9 @@ global.eg = '╰⊱💚⊱ *𝙀́𝙓𝙄𝙏𝙊 | 𝙎𝙐𝘾𝘾𝙀𝙎�
 global.exito = eg
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
-global.wm = " : 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨"
-global.igfg = "𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿"
-global.nomorown = "593993684821"
+global.wm = " 𝙵𝙴𝙽𝙸𝚇-𝙱𝙾𝚃: 𝙵𝙴𝙽𝙸𝚇"
+global.igfg = "ＦＥＮＩＸ－ＢＯＴ－ＭＤ"
+global.nomorown = "51995490187"
 global.pdoc = ["application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.ms-excel", "application/msword", "application/pdf", "text/rtf"]
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
